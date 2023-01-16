79594a9b7d964a41be68b0227cbf2bcf3cb59312
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2023-01-16 22:02:01 +1100

    add NER/topic classification to experimental fastapi endpoints

--- api.py
+++ api.py
@@ -3,6 +3,48 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+MAXIMUM_CACHE_SIZE = 10
+
+
+class LRUCache:
+    def __init__(self, capacity):
+        # From https://www.kunxi.org/2014/05/lru-cache-in-python/
+        self.capacity = capacity
+        self.tm = 0
+        self.cache = {}
+        self.lru = {}
+
+    def __getitem__(self, key):
+        if key in self.cache:
+            self.lru[key] = self.tm
+            self.tm += 1
+            return self.cache[key]
+        return -1
+
+    def __setitem__(self, key, value):
+        if len(self.cache) >= self.capacity:
+            # find the LRU entry
+            old_key = min(self.lru.keys(), key=lambda k:self.lru[k])
+            self.cache.pop(old_key)
+            self.lru.pop(old_key)
+        self.cache[key] = value
+        self.lru[key] = self.tm
+        self.tm += 1
+
+
+_cache = LRUCache(capacity=MAXIMUM_CACHE_SIZE)
+
+
+class CachingDict:
+    def __init__(self, unique_key: str, backing_dict: dict):
+        self.__unique_key = unique_key
+        self.__backing_dict = backing_dict
+    
+    def __getitem__(self, item):
+        if (self.__unique_key, item) not in _cache:
+            print("Creating key:", self.__unique_key, item)
+            _cache[self.__unique_key, item] = self.__backing_dict[item]()
+        return _cache[self.__unique_key, item]
 
 
 #===========================================================#
@@ -11,27 +53,27 @@ app = FastAPI()
 
 
 dp = Pororo(task="dep_parse", lang="ko")
-pos = {
-    'ko': Pororo(task="pos", lang="ko"),
-    'ja': Pororo(task="pos", lang="ja"),
-    'en': Pororo(task="pos", lang="en"),
-    'zh': Pororo(task="pos", lang="zh"),
-}
-ner = {
-    'en': Pororo(task="ner", lang="en"),
-    'ko': Pororo(task="ner", lang="ko"),
-    'zh': Pororo(task="ner", lang="zh"),
-    'ja': Pororo(task="ner", lang="ja"),
-}
-sa = {
-    'ja': {
+pos = CachingDict('pos', {
+    'ko': lambda: Pororo(task="pos", lang="ko"),
+    'ja': lambda: Pororo(task="pos", lang="ja"),
+    'en': lambda: Pororo(task="pos", lang="en"),
+    'zh': lambda: Pororo(task="pos", lang="zh"),
+})
+ner = CachingDict('ner', {
+    'en': lambda: Pororo(task="ner", lang="en"),
+    'ko': lambda: Pororo(task="ner", lang="ko"),
+    'zh': lambda: Pororo(task="ner", lang="zh"),
+    'ja': lambda: Pororo(task="ner", lang="ja"),
+})
+sa = CachingDict('sa', {
+    'ja': lambda: {
         "Internal Data": Pororo(task="sentiment", lang="ja")
     },
-    'ko': {
+    'ko': lambda: {
         "Shopping Reviews": Pororo(task="sentiment", model="brainbert.base.ko.nsmc", lang="ko"),
         "Movie Reviews": Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko"),
     }
-}
+})
 
 
 @app.get("/dependencyParse")
@@ -39,13 +81,13 @@ async def _(sentences: str):
     return dp(sentences)
 
 
-#@app.get("/namedEntityRecognition")
-#async def _(iso: str, sentences: str):
-#    if iso == 'ko':
-#        # Can't imagine a situation where word sense disambiguation wouldn't 
-#        # be preferred if it's available for Korean - always enable
-#        return ner[iso](sentences, apply_wsd=True)
-#    return ner[iso](sentences)
+@app.get("/namedEntityRecognition")
+async def _(iso: str, sentences: str):
+    if iso == 'ko':
+        # Can't imagine a situation where word sense disambiguation wouldn't 
+        # be preferred if it's available for Korean - always enable
+        return ner[iso](sentences, apply_wsd=True)
+    return ner[iso](sentences)
 
 
 @app.get("/partOfSpeech")
@@ -66,27 +108,27 @@ async def _(iso: str, sentences: str):
 #===========================================================#
 
 
-#zsl = {
-#    'en': Pororo(task="zero-topic"),
-#    'ko': Pororo(task="zero-topic", lang="ko"),
-#    'ja': Pororo(task="zero-topic", lang="ja"),
-#    'zh': Pororo(task="zero-topic", lang="zh"),
-#}
-#nli = {
-#    'ko': Pororo(task="nli", lang="ko"),
-#    'ja': Pororo(task="nli", lang="ja"),
-#    'zh': Pororo(task="nli", lang="zh"),
-#    'en': Pororo(task="nli", lang="en"),
-#}
+zsl = CachingDict('zsl', {
+    'en': lambda: Pororo(task="zero-topic"),
+    'ko': lambda: Pororo(task="zero-topic", lang="ko"),
+    'ja': lambda: Pororo(task="zero-topic", lang="ja"),
+    'zh': lambda: Pororo(task="zero-topic", lang="zh"),
+})
+nli = CachingDict('nli', {
+    'ko': lambda: Pororo(task="nli", lang="ko"),
+    'ja': lambda: Pororo(task="nli", lang="ja"),
+    'zh': lambda: Pororo(task="nli", lang="zh"),
+    'en': lambda: Pororo(task="nli", lang="en"),
+})
 
-#@app.get("/zeroShotTopicClassification")
-#async def _(iso: str, sentences: str):
-#    return zsl[iso](sentences)
+@app.get("/zeroShotTopicClassification")
+async def _(iso: str, sentences: str):
+    return zsl[iso](sentences)
 
 
-#@app.get("/naturalLanguageInference")
-#async def _(iso: str, sentences: str):
-#    return nli[iso](sentences)
+@app.get("/naturalLanguageInference")
+async def _(iso: str, sentences: str):
+    return nli[iso](sentences)
 
 
 #===========================================================#
@@ -94,12 +136,12 @@ async def _(iso: str, sentences: str):
 #===========================================================#
 
 
-g2p = {
-    'ko': Pororo(task="g2p", lang="ko"),
-    'en': Pororo(task="g2p", lang="en"),
-    'zh': Pororo(task="g2p", lang="zh"),
-    'ja': Pororo(task="g2p", lang="ja"),
-}
+g2p = CachingDict('g2p', {
+    'ko': lambda: Pororo(task="g2p", lang="ko"),
+    'en': lambda: Pororo(task="g2p", lang="en"),
+    'zh': lambda: Pororo(task="g2p", lang="zh"),
+    'ja': lambda: Pororo(task="g2p", lang="ja"),
+})
 
 
 @app.get("/graphemeToPhoneme")
@@ -112,18 +154,18 @@ async def _(iso: str, sentences: str):
 #===========================================================#
 
 
-inflection = {
-    'ko': Pororo(task="inflection", lang="ko"),
-    'en': Pororo(task="inflection", lang="en"),
-    'ja': Pororo(task="inflection", lang="ja"),
-}
+inflection = CachingDict('inflection', {
+    'ko': lambda: Pororo(task="inflection", lang="ko"),
+    'en': lambda: Pororo(task="inflection", lang="en"),
+    'ja': lambda: Pororo(task="inflection", lang="ja"),
+})
 ocr = Pororo(task="ocr", lang="ko")
-col = {
-    'ko': Pororo(task="col", lang="ko"),
-    'ja': Pororo(task="collocation", lang="ja"),
-    'en': Pororo(task="col", lang="en"),
-    'zh': Pororo(task="col", lang="zh"),
-}
+col = CachingDict('col', {
+    'ko': lambda: Pororo(task="col", lang="ko"),
+    'ja': lambda: Pororo(task="collocation", lang="ja"),
+    'en': lambda: Pororo(task="col", lang="en"),
+    'zh': lambda: Pororo(task="col", lang="zh"),
+})
 
 @app.get("/morphologicalInflection")
 async def _(iso: str, word: str):
@@ -138,5 +180,4 @@ async def _(image_data: str):
 @app.get("/collocation")
 async def _(iso: str, word: str):
     return col[iso](word, detail=True)
-
 
